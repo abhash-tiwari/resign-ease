ac16571c119eefb2a58b5c5b7674a2019a3b7715
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2025-03-09 00:35:41 +0530

    Few Change

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <footer className="footer sm:footer-horizontal footer-center bg-gray-800 text-gray-200 p-4">
+      <footer className="footer sm:footer-horizontal footer-center bg-gray-900 text-gray-200 p-4">
         <aside className="flex flex-col items-center gap-2">
           <p className="text-sm font-medium">
             Copyright © {new Date().getFullYear()} - All rights reserved 
