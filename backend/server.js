328a2c45c3b51f89dd551679cc4aae846cdafc2f
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2025-03-09 00:25:50 +0530

    Complete Backend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Connect to Database
+connectDB();
+
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/user', require('./routes/user'));
+app.use('/api/admin', require('./routes/admin'));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
